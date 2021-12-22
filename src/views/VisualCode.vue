4f6639b1e85b2c3aace3abcb4cda7b81a826fab4
CarlosDiasJr <diascarlosjr@gmail.com>
2021-12-22 15:15:04 -0300

    add Condicionais, geração de código arduino melhorada

--- src/views/VisualCode.vue
+++ src/views/VisualCode.vue
@@ -34,6 +34,13 @@
                   @click="add_to_code(3, '5', 0, 'Escrever saída PWM')"
                   >Escrever saída PWM</b-button
                 >
+                <b-button
+                  type="is-info"
+                  class="box"
+                  expanded
+                  @click="add_to_code(4, '>', 0, 'Se ...')"
+                  >Condição</b-button
+                >
               </div>
             </b-tab-item>
             <b-tab-item label="Robô"></b-tab-item>
@@ -42,11 +49,10 @@
         </section>
       </div>
       <div class="column has-background-light has-text-white box">
-        <draggable v-model="data_current">
-          <transition-group>
+        <draggable group="blocos" :list="data_current" @change="log">
+          <div v-for="element in data_current" :key="'btni_' + element.id">
             <div
-              v-for="element in data_current"
-              :key="'btni_' + element.id"
+              v-if="element.type < 4"
               :class="{
                 box: true,
                 'is-write': element.type >= 2,
@@ -88,12 +94,116 @@
                 </b-icon>
               </a>
             </div>
-          </transition-group>
+            <!-- Caso bloco de IF -->
+            <div v-if="element.type == 4">
+              <div class="box is-condition">
+                Se o valor for
+                <b
+                  style="cursor: pointer"
+                  @click="
+                    get_new_value(
+                      element,
+                      'pin',
+                      'Qual condição deseja utilizar?'
+                    )
+                  "
+                  >{{ element.pin }}</b
+                >
+
+                <b
+                  style="cursor: pointer"
+                  @click="
+                    get_new_value(
+                      element,
+                      'value',
+                      'Qual o novo valor para escrever no pino?'
+                    )
+                  "
+                >
+                  {{ element.value }}</b
+                >
+                <a
+                  @click="remove_from_code(element.id)"
+                  style="color: rgba(0, 0, 0, 0.25)"
+                >
+                  <b-icon
+                    style="float: right"
+                    pack="fas"
+                    icon="trash"
+                    size="is-small"
+                  >
+                  </b-icon>
+                </a>
+              </div>
+              <div class="conditional-block">
+                <draggable
+                  class="conditional-block-area"
+                  @change="log"
+                  :list="element.blocks"
+                  group="blocos"
+                >
+                  <div
+                    v-for="inner_element in element.blocks"
+                    :key="'btni_inner_' + inner_element.id"
+                  >
+                    <div
+                      v-if="inner_element.type < 4"
+                      :class="{
+                        box: true,
+                        'is-write': inner_element.type >= 2,
+                        'is-read': inner_element.type < 2,
+                      }"
+                    >
+                      {{ inner_element.label }} no pino
+                      <b
+                        style="cursor: pointer"
+                        @click="
+                          get_new_value(
+                            inner_element,
+                            'pin',
+                            'Qual o pino deseja utilizar?'
+                          )
+                        "
+                        >{{ inner_element.pin }}</b
+                      >
+                      <span v-if="inner_element.type >= 2"
+                        >, o valor
+                        <b
+                          style="cursor: pointer"
+                          @click="
+                            get_new_value(
+                              inner_element,
+                              'value',
+                              'Qual o novo valor para escrever no pino?'
+                            )
+                          "
+                          >{{ inner_element.value }}</b
+                        ></span
+                      >
+                      <a
+                        @click="remove_from_code(inner_element.id)"
+                        style="color: rgba(0, 0, 0, 0.25)"
+                      >
+                        <b-icon
+                          style="float: right"
+                          pack="fas"
+                          icon="trash"
+                          size="is-small"
+                        >
+                        </b-icon>
+                      </a>
+                    </div>
+                  </div>
+                </draggable>
+              </div>
+            </div>
+            <!-- FIM do Caso bloco IF -->
+          </div>
         </draggable>
       </div>
       <div
-        class="column is-two-fifth"
-        style="padding: 30px"
+        class="column is-one-quarter"
+        style="padding: 40px"
         v-if="show_arduinoCode"
       >
         <p v-html="arduino_code"></p>
@@ -118,8 +228,10 @@ export default {
     };
   },
   methods: {
+    log: function (evt) {
+      window.console.log(evt);
+    },
     get_new_value(variable, propety, label) {
-      console.log(variable, propety, label);
       this.$buefy.dialog.prompt({
         message: label,
         inputAttrs: {
@@ -130,15 +242,20 @@ export default {
       });
     },
     add_to_code(type, pin, value, label) {
+      let total_blocos = this.data_current.length;
+      this.data_current.forEach(function (elemento) {
+        total_blocos += elemento.blocks.length;
+      });
+      console.log("blocos na tela:", total_blocos);
       this.data_current.push({
-        id: this.data_current.length,
+        id: total_blocos,
         type: type,
         pin: pin,
         value: value,
         label: label,
+        blocks: [],
         var: "variavel_" + this.data_current.length,
       });
-      console.log(this.data_current.nodes);
     },
     remove_from_code(id) {
       this.data_current.splice(
@@ -163,7 +280,9 @@ export default {
       return JSON.stringify(this.$store.state.current_file, null, 2);
     },
     arduino_code: function () {
-      return this.$store.state.arduino_code.replace(/\n/g, "<br />");
+      return this.$store.state.arduino_code
+        .replace(/\n/g, "<br />")
+        .replace(/\t/g, "&emsp;");
     },
     current_file: function () {
       return this.$store.state.current_file.content;
@@ -178,20 +297,58 @@ export default {
         const element = this.data_current[index];
         if (element.type == 0)
           this.$store.state.arduino_code +=
-            "    bool " +
-            element.var +
-            " = digitalRead(" +
-            element.pin +
-            ");\n";
+            "\tbool " + element.var + " = digitalRead(" + element.pin + ");\n";
         else if (element.type == 1)
           this.$store.state.arduino_code +=
-            "    int " + element.var + " = analogRead(" + element.pin + ");\n";
+            "\tint " + element.var + " = analogRead(" + element.pin + ");\n";
         else if (element.type == 2)
           this.$store.state.arduino_code +=
-            "    digitalWrite(" + element.pin + "," + element.value + ");\n";
+            "\tdigitalWrite(" + element.pin + "," + element.value + ");\n";
         else if (element.type == 3)
           this.$store.state.arduino_code +=
-            "    analogWrite(" + element.pin + "," + element.value + ");\n";
+            "\t analogWrite(" + element.pin + "," + element.value + ");\n";
+        else if (element.type == 4) {
+          if (index > 0) {
+            if (this.data_current[index - 1].type < 2) {
+              let variable = this.data_current[index - 1].var;
+              this.$store.state.arduino_code +=
+                "\tif(" + variable + element.pin + element.value + "){\n";
+
+              for (let j = 0; j < element.blocks.length; j++) {
+                if (element.blocks[j].type == 0)
+                  this.$store.state.arduino_code +=
+                    "\t\t bool " +
+                    element.blocks[j].var +
+                    " = digitalRead(" +
+                    element.blocks[j].pin +
+                    ");\n";
+                else if (element.blocks[j].type == 1)
+                  this.$store.state.arduino_code +=
+                    "\t\t int " +
+                    element.blocks[j].var +
+                    " = analogRead(" +
+                    element.blocks[j].pin +
+                    ");\n";
+                else if (element.blocks[j].type == 2)
+                  this.$store.state.arduino_code +=
+                    "\t\t digitalWrite(" +
+                    element.blocks[j].pin +
+                    "," +
+                    element.blocks[j].value +
+                    ");\n";
+                else if (element.blocks[j].type == 3)
+                  this.$store.state.arduino_code +=
+                    "\t\t analogWrite(" +
+                    element.blocks[j].pin +
+                    "," +
+                    element.blocks[j].value +
+                    ");\n";
+              }
+
+              this.$store.state.arduino_code += "\n\t}";
+            }
+          }
+        }
       }
       this.$store.state.arduino_code += "\n}";
     },
@@ -212,14 +369,36 @@ export default {
 }
 .is-write {
   background-color: #f14668;
-  max-width: 30vw;
+  max-width: 40vw;
   color: #fff;
   margin: 10px;
 }
 .is-read {
   background-color: #ffe08a;
-  max-width: 30vw;
+  max-width: 40vw;
   color: rgba(0, 0, 0, 0.7);
   margin: 10px;
+}
+.is-condition {
+  background-color: #3488ce;
+  max-width: 40vw;
+  color: #fff;
+  margin: 10px;
+}
+.conditional-block {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+  background-color: #eff5fb;
+  max-width: 40vw;
+  min-height: 100px;
+  color: rgba(0, 0, 0, 0.7);
+  margin: 10px;
+  padding: 10px;
+}
+.conditional-block-area {
+  max-width: 30vw;
+  min-height: 100px;
+  color: rgba(0, 0, 0, 0.7);
+  margin-left: auto;
 }
 </style>
